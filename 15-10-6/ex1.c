2e0895c861d0f734c2878f522a9d38af441d9765
Maybe <maytryark@gmail.com>
2015-10-07 00:48:54 +0900

    create ex1.c+Makefile

---
+++ 15-10-6/ex1.c
@@ -0,0 +1,13 @@
+//
+//	10/7/15 Maybe
+//	maytryark@gmail.com
+//
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+	int distance = 100;
+
+	printf("You are %d miles away.\n",distance);
+	return 0;
+}
