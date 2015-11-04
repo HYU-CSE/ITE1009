461e828b71ff004d8ca0ea64ebf365eed17530b8
Maybe <maytryark@gmail.com>
2015-11-04 14:05:52 +0900

    15-11-04

--- 15-11-04/list.c
+++ 15-11-04/list.c
@@ -1,4 +1,3 @@
-
 #include "list.h"
 #include "dbg.h"
 List *List_create( )
@@ -6,23 +5,20 @@ List *List_create( )
 return calloc( 1, sizeof( List) ) ;
 }
 void List_destroy( List * list)
- {
-	LIST_FOREACH( list, first, next, cur) 
-	{
-		if ( cur-> prev) 
-		{
-			free( cur-> prev) ;
-		}
-	}
-	free( list-> last) ;
-	free( list) ;
+{
+LIST_FOREACH( list, first, next, cur) {
+if ( cur-> prev) {
+free( cur-> prev) ;
+}
+}
+free( list-> last) ;
+free( list) ;
 }
 void List_clear( List * list)
 {
-	LIST_FOREACH( list, first, next, cur) 
-	{
-	free( cur-> value) ;
-	}
+LIST_FOREACH( list, first, next, cur) {
+free( cur-> value) ;
+}
 }
 void List_clear_destroy( List * list)
 {
@@ -75,36 +71,31 @@ return node != NULL ? List_remove( list, node) : NULL;
 }
 void *List_remove( List * list, ListNode * node)
 {
-	void *result = NULL;
-	check( list-> first && list-> last, "List is empty.") ;
-	check( node, "node can't be NULL") ;
-		if ( node == list-> first && node == list-> last) 
-		{
-			list-> first = NULL;
-			list-> last = NULL;
-		} 
-		else if ( node == list-> first) 
-		{
-		list-> first = node-> next;
-		check( list-> first != NULL, "Invalid list, somehow got a first that is NULL.") ;
-		list-> first-> prev = NULL;
-	 	} 
-		else if ( node == list-> last) 
-		{
-		list-> last = node-> prev;
-		check( list-> last != NULL, "Invalid list, somehow got a next that is NULL.") ;
-		list-> last-> next = NULL;
-		} 
-		else 
-		{
-			ListNode *after = node-> next;
-			ListNode *before = node-> prev;
-			after-> prev = before;
-			before-> next = after;
-		}
-	list-> count--;
-	result = node-> value;
-	free( node) ;
-	error:
-	return result;
+void *result = NULL;
+check( list-> first && list-> last, "List is empty.") ;
+check( node, "node can't be NULL") ;
+if ( node == list-> first && node == list-> last) {
+list-> first = NULL;
+list-> last = NULL;
+} else if ( node == list-> first) {
+list-> first = node-> next;
+check( list-> first != NULL,
+"Invalid list, somehow got a first that is NULL.") ;
+list-> first-> prev = NULL;
+} else if ( node == list-> last) {
+list-> last = node-> prev;
+check( list-> last != NULL,
+"Invalid list, somehow got a next that is NULL.") ;
+list-> last-> next = NULL;
+} else {
+ListNode *after = node-> next;
+ListNode *before = node-> prev;
+after-> prev = before;
+before-> next = after;
+}
+list-> count--;
+result = node-> value;
+free( node) ;
+error:
+return result;
 }
