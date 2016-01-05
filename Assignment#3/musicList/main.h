21cef2fb68fcb4c5cdc584d2eac163c4bb38d1fa
May be <May be>
2016-01-05 23:51:37 +0900

    Edit: project name

---
+++ Assignment#3/musicList/main.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef WIN32
+	#include <Windows.h>
+	#pragma warning (disable : 4996)
+#elif
+
+#endif
+
+#include <stdlib.h>
+
+#include "resource.h"
+
+#include "FileDialog.h"
+
+#include "drawing.h"
+
+#define SIZEW 400
+#define SIZEH 600
+#define SIZEWW SIZEW + 16
+#define SIZEHH SIZEH + 39
+
+static POINT point;
+
+using namespace std;
